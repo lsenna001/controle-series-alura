7c338857e1b5d3c888fbc9d38b510962f145df94
Leonardo Sena <leonardosenamesquita@outlook.com.br>
2024-05-31 17:45:12 -0300

    Ajustando migrates para implementação de temporadas e episódios

--- database/migrations/2024_05_31_201425_create_episodes_table.php
+++ database/migrations/2024_05_31_201425_create_episodes_table.php
@@ -13,7 +13,8 @@ return new class extends Migration
     {
         Schema::create('episodes', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('season_id')->constrained();
+            $table->foreignId('season_id')->constrained()->onDelete('cascade');
+            $table->unsignedTinyInteger('number');
         });
     }
 
