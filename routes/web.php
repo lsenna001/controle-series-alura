34b986f9b04ca6f3d18f7dc83a0b81967df4be58
Leonardo Sena <leonardosenamesquita@outlook.com.br>
2024-05-31 16:09:34 -0300

    Implementação do Delete de Séries

--- routes/web.php
+++ routes/web.php
@@ -4,10 +4,10 @@ use App\Http\Controllers\SeriesController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/series');
 });
 
-Route::get('/series', [SeriesController::class, 'index']);
-Route::get('/series/create', [SeriesController::class, 'create']);
-Route::post('/series/store', [SeriesController::class, 'store']);
+Route::resource('/series', SeriesController::class)
+->only(['index', 'create', 'store', 'destroy']);
+
 
