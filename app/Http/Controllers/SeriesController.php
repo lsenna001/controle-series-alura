9ca222db456511c344e78ae039f5c4ca5499041c
Leonardo Sena <leonardosenamesquita@outlook.com.br>
2024-05-30 22:41:40 -0300

    Cadastro de Séries

--- app/Http/Controllers/SeriesController.php
+++ app/Http/Controllers/SeriesController.php
@@ -3,16 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Serie;
 
 class SeriesController extends Controller
 {
     public function index(Request $request)
     {
-        $series = [
-            'Punisher',
-            'Lost',
-            'Grey\'s Anatomy'
-        ];
+        $series = Serie::query()->orderBy('nome')->get();
 
         return view('series.index')->with('series', $series);
     }
@@ -20,5 +17,15 @@ class SeriesController extends Controller
     public function create()
     {
         return view('series.create');
+    }
+
+    public function store(Request $request)
+    {
+        $nomeSerie = $request->input('nome');
+        $serie = new Serie();
+        $serie->nome = $nomeSerie;
+        $serie->save();
+
+        return redirect('/series');
     }
 }
